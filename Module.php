67e69814100b703d2f0919b2d3311ab3912bb702
John Flatness <john@zerocrates.org>
2021-03-21 16:13:49 -0400

    Add titles to sets in item API output

--- Module.php
+++ Module.php
@@ -27,6 +27,7 @@ class Module extends AbstractModule
         $json = $e->getParam('jsonLd');
         $itemRep = $e->getTarget();
         $thumbnailsById = [];
+        $setTitlesById = [];
 
         foreach ($itemRep->media() as $media) {
             $thumbnailsById[$media->id()] = $media->thumbnailUrls();
@@ -36,6 +37,17 @@ class Module extends AbstractModule
             $id = $mediaRef->id();
             $json['o:media'][$index] = new ReferenceProxy($mediaRef, 
                 ['thumbnail_urls' => $thumbnailsById[$id]]
+            );
+        }
+
+        foreach ($itemRep->itemSets() as $set) {
+            $setTitlesById[$set->id()] = (string) $set->value('dcterms:title');
+        }
+
+        foreach ($json['o:item_set'] as $index => $setRef) {
+            $id = $setRef->id();
+            $json['o:item_set'][$index] = new ReferenceProxy($setRef,
+                ['title' => $setTitlesById[$id]]
             );
         }
         $e->setParam('jsonLd', $json);
